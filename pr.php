0bef4df4ed26aff827af9b8a37cdc42efb490882
risyadul <48561451+risyadul@users.noreply.github.com>
2019-05-07 14:32:18 +0700

    Add files via upload

---
+++ pr.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<p>Orang pendiam tidak sepantasnya dijauhi maupun dianggap aneh. Karena tidak selamanya orang yang pendiam adalah pecandu narkoba, berkepribadian sombong, penderita kelainan jiwa, atau seorang teroris. Orang pendiam hanyalah orang yang tidak pandai banyak bicara sekaligus tidak terlalu senang berada di tempat ramai terlalu lama.</p>
+
+
+	<?php 
+
+	$array = [
+		["orang" => "Orang, Orang, Orang (3)", "tidak" => "Tidak, Tidak, Tidak, Tidak (4)", "yang" => "Yang, Yang (2)"],
+		["yeye" => "kaalll", "asd" => "adsdsa", "lokas" => "aser"]
+	];
+	var_dump($array);
+	// foreach ($array as $ase) {
+	// 	echo $ase[1] . "<br>";
+	// }
+
+	
+
+
+
+
+	 ?>
+</body>
+</html>
